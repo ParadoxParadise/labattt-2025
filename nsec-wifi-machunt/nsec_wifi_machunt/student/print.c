e68615287a7ec32f1c293429b31a99795ba6e7b8
Nguyễn Đức Hùng <tuantu123x@gmail.com>
2025-08-18 13:49:39 +0700

    a

--- nsec-wifi-machunt/nsec_wifi_machunt/student/print.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	char input[100];
-	printf("Enter target's name: ");
-	fgets(input, sizeof(input), stdin);
-	printf("%s", input);
-	return 0;
-}
--- ptit-ettercap/ptit-ettercap/attacker/_bin/fixlocal.sh
+++
@@ -1,3 +0,0 @@
-sudo sysctl net.ipv4.conf.all.forwarding=0
-sudo chmod 777 /home/ubuntu/logging
-echo -n "Mail: " | cat - /home/ubuntu/.local/.email > /home/ubuntu/mail.txt
